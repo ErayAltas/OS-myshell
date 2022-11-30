7c8f14898923806ca6e6da18ecc90db46f9965cb
mustafaozcan <mustafaozcan@routin.com.tr>
2022-11-30 18:31:27 +0300

    updated

--- execx.c
+++ execx.c
@@ -9,18 +9,23 @@
 int main(int argc, char *argv[])
 {
     int forkVal, execVal;
+    // if writef called with execx, it executes writef number of times
+    if (strcmp(argv[3], "writef") == 0 && !strcmp(argv[4], "-f") && argv[5] != NULL && argc == 6)
+    {
 
-    // if clear called with execx, it executes clear number of times
-    if (strcmp(argv[3], "clear") == 0)
-    {
-        clear();
-    }
-    // if ls called with execx, it executes ls number of times
-    else if (strcmp(argv[3], "ls") == 0)
-    {
         for (int i = 0; i < atoi(argv[2]); i++)
         {
-            system("/bin/ls");
+            forkVal = fork();
+            if (forkVal == 0)
+            {
+                execVal = execve("writef", argv, NULL);
+                exit(0);
+                perror("exec error");
+            }
+            else
+            {
+                wait(&execVal);
+            }
         }
     }
     // if cat called with execx, it executes cat number of times
@@ -59,33 +64,25 @@ int main(int argc, char *argv[])
             system("/bin/bash");
         }
     }
+    // if clear called with execx, it executes clear number of times
+    else if (strcmp(argv[3], "clear") == 0)
+    {
+        clear();
+    }
+    // if ls called with execx, it executes ls number of times
+    else if (strcmp(argv[3], "ls") == 0)
+    {
+        for (int i = 0; i < atoi(argv[2]); i++)
+        {
+            system("/bin/ls");
+        }
+    }
     // if exit called with execx, it executes exit number of times
     else if (strcmp(argv[3], "exit") == 0)
     {
-        for (int i = 0; i < atoi(argv[2]); i++)
-        {
-            exit(0);
-        }
+        exit(0);
     }
-    // if writef called with execx, it executes writef number of times
-    else if (strcmp(argv[3], "writef") == 0 && !strcmp(argv[4], "-f") && argv[5] != NULL && argc == 6)
-    {
-
-        for (int i = 0; i < atoi(argv[2]); i++)
-        {
-            forkVal = fork();
-            if (forkVal == 0)
-            {
-                execVal = execve("writef", argv, NULL);
-                exit(0);
-                perror("exec error");
-            }
-            else
-            {
-                wait(&execVal);
-            }
-        }
-    }
+    
     else
     {
         printf("Wrong command. !help to see commands.\n");
